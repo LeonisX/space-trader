c34f38028e8d1966e07ec947b012ffb0128ac1f4
Leonis <tv-games@mail.ru>
2018-10-28 14:12:52 +0200

    Russian translation for TargetSystemPanel

--- src/main/java/spacetrader/gui/TargetSystemPanel.java
+++ src/main/java/spacetrader/gui/TargetSystemPanel.java
@@ -2,6 +2,7 @@ package spacetrader.gui;
 
 import spacetrader.controls.*;
 import spacetrader.controls.enums.AnchorStyles;
+import spacetrader.controls.enums.ControlBinding;
 import spacetrader.controls.enums.FlatStyle;
 import spacetrader.game.*;
 import spacetrader.util.Functions;
@@ -27,8 +28,8 @@ class TargetSystemPanel extends Panel {
     private Label targetTechLevelLabelValue = new Label();
     private Label targetGovernmentLabel = new Label();
     private Label targetGovernmentLabelValue = new Label();
-    private Label targetResourceLabel = new Label();
-    private Label targetResourceLabelValue = new Label();
+    private Label targetResourcesLabel = new Label();
+    private Label targetResourcesLabelValue = new Label();
     private Label targetPoliceLabel = new Label();
     private Label targetPoliceLabelValue = new Label();
     private Label targetPiratesLabel = new Label();
@@ -39,6 +40,13 @@ class TargetSystemPanel extends Panel {
 
     private Button trackButton = new Button();
     private Button warpButton = new Button();
+    
+    private Label[] labels = new Label[]{targetNameLabel, targetSizeLabel, targetTechLevelLabel,
+            targetGovernmentLabel, targetResourcesLabel, targetPoliceLabel, targetPiratesLabel, targetDistanceLabel};
+
+    private Label[] labelValues = new Label[]{targetNameLabelValue, targetSizeLabelValue, targetTechLevelLabelValue,
+            targetGovernmentLabelValue, targetResourcesLabelValue, targetPoliceLabelValue, targetPiratesLabelValue,
+            targetDistanceLabelValue};
 
     TargetSystemPanel(SpaceTrader mainWindow) {
         this.mainWindow = mainWindow;
@@ -54,11 +62,12 @@ class TargetSystemPanel extends Panel {
         anchor = AnchorStyles.TOP_RIGHT;
 
         setText("Target System");
-        setSize(216, 168);
+        setSize(219, 173);
         setTabStop(false);
 
-        prevSystemButton.setFlatStyle(FlatStyle.FLAT);
-        prevSystemButton.setLocation(160, 16);
+        prevSystemButton.setAutoWidth(true);
+        prevSystemButton.setControlBinding(ControlBinding.LEFT);
+        prevSystemButton.setLocation(164, 21);
         prevSystemButton.setSize(18, 18);
         prevSystemButton.setTabIndex(57);
         prevSystemButton.setText("<");
@@ -69,8 +78,10 @@ class TargetSystemPanel extends Panel {
             }
         });
 
+        nextSystemButton.setAutoWidth(true);
+        nextSystemButton.setControlBinding(ControlBinding.RIGHT);
         nextSystemButton.setFlatStyle(FlatStyle.FLAT);
-        nextSystemButton.setLocation(186, 16);
+        nextSystemButton.setLocation(190, 21);
         nextSystemButton.setSize(18, 18);
         nextSystemButton.setTabIndex(58);
         nextSystemButton.setText(">");
@@ -81,122 +92,87 @@ class TargetSystemPanel extends Panel {
             }
         });
 
-        targetNameLabel.setAutoSize(true);
-        targetNameLabel.setFont(FontCollection.bold825);
-        targetNameLabel.setLocation(8, 16);
-        targetNameLabel.setSize(39, 16);
-        targetNameLabel.setTabIndex(0);
+        targetNameLabel.setLocation(8, 21);
+        //targetNameLabel.setSize(39, 16);
         targetNameLabel.setText("Name:");
 
-        targetNameLabelValue.setLocation(88, 16);
-        targetNameLabelValue.setSize(65, 13);
-        targetNameLabelValue.setTabIndex(1);
+        targetNameLabelValue.setLocation(88, 21);
+        //targetNameLabelValue.setSize(65, 13);
         //targetNameLabelValue.setText("Tarchannen");
 
-        targetSizeLabel.setAutoSize(true);
-        targetSizeLabel.setFont(FontCollection.bold825);
-        targetSizeLabel.setLocation(8, 32);
-        targetSizeLabel.setSize(31, 16);
-        targetSizeLabel.setTabIndex(2);
+        targetSizeLabel.setLocation(8, 37);
+        //targetSizeLabel.setSize(31, 16);
         targetSizeLabel.setText("Size:");
 
-        targetSizeLabelValue.setLocation(88, 32);
-        targetSizeLabelValue.setSize(45, 13);
-        targetSizeLabelValue.setTabIndex(14);
+        targetSizeLabelValue.setLocation(88, 37);
+        //targetSizeLabelValue.setSize(45, 13);
         //targetSizeLabelValue.setText("Medium");
 
-        targetTechLevelLabel.setAutoSize(true);
-        targetTechLevelLabel.setFont(FontCollection.bold825);
-        targetTechLevelLabel.setLocation(8, 48);
-        targetTechLevelLabel.setSize(65, 16);
-        targetTechLevelLabel.setTabIndex(3);
+        targetTechLevelLabel.setLocation(8, 53);
+        //targetTechLevelLabel.setSize(65, 16);
         targetTechLevelLabel.setText("Tech Level:");
 
-        targetTechLevelLabelValue.setLocation(88, 48);
-        targetTechLevelLabelValue.setSize(82, 13);
-        targetTechLevelLabelValue.setTabIndex(13);
+        targetTechLevelLabelValue.setLocation(88, 53);
+        //targetTechLevelLabelValue.setSize(82, 13);
         //targetTechLevelLabelValue.setText("Pre-Agricultural");
 
-        targetGovernmentLabel.setAutoSize(true);
-        targetGovernmentLabel.setFont(FontCollection.bold825);
-        targetGovernmentLabel.setLocation(8, 64);
-        targetGovernmentLabel.setSize(72, 16);
-        targetGovernmentLabel.setTabIndex(4);
+        targetGovernmentLabel.setLocation(8, 69);
+        //targetGovernmentLabel.setSize(72, 16);
         targetGovernmentLabel.setText("Government:");
 
-        targetGovernmentLabelValue.setLocation(88, 64);
-        targetGovernmentLabelValue.setSize(91, 13);
-        targetGovernmentLabelValue.setTabIndex(15);
+        targetGovernmentLabelValue.setLocation(88, 69);
+        //targetGovernmentLabelValue.setSize(91, 13);
         //targetGovernmentLabelValue.setText("Communist State");
 
-        targetResourceLabel.setAutoSize(true);
-        targetResourceLabel.setFont(FontCollection.bold825);
-        targetResourceLabel.setLocation(8, 80);
-        targetResourceLabel.setSize(58, 16);
-        targetResourceLabel.setTabIndex(5);
-        targetResourceLabel.setText("Resource:");
+        targetResourcesLabel.setLocation(8, 85);
+        //targetResourcesLabel.setSize(58, 16);
+        targetResourcesLabel.setText("Resource:");
 
-        targetResourceLabelValue.setLocation(88, 80);
-        targetResourceLabelValue.setSize(105, 13);
-        targetResourceLabelValue.setTabIndex(9);
-        //targetResourceLabelValue.setText("Sweetwater Oceans");
+        targetResourcesLabelValue.setLocation(88, 85);
+        //targetResourcesLabelValue.setSize(105, 13);
+        //targetResourcesLabelValue.setText("Sweetwater Oceans");
 
-        targetPoliceLabel.setAutoSize(true);
-        targetPoliceLabel.setFont(FontCollection.bold825);
-        targetPoliceLabel.setLocation(8, 96);
-        targetPoliceLabel.setSize(40, 16);
-        targetPoliceLabel.setTabIndex(6);
+        targetPoliceLabel.setLocation(8, 101);
+        //targetPoliceLabel.setSize(40, 16);
         targetPoliceLabel.setText("Police:");
 
-        targetPoliceLabelValue.setLocation(88, 96);
-        targetPoliceLabelValue.setSize(53, 13);
-        targetPoliceLabelValue.setTabIndex(10);
+        targetPoliceLabelValue.setLocation(88, 101);
+        //targetPoliceLabelValue.setSize(53, 13);
         //targetPoliceLabelValue.setText("Abundant");
 
-        targetPiratesLabel.setAutoSize(true);
-        targetPiratesLabel.setFont(FontCollection.bold825);
-        targetPiratesLabel.setLocation(8, 112);
-        targetPiratesLabel.setSize(44, 16);
-        targetPiratesLabel.setTabIndex(7);
+        targetPiratesLabel.setLocation(8, 117);
+        //targetPiratesLabel.setSize(44, 16);
         targetPiratesLabel.setText("Pirates:");
 
-        targetPiratesLabelValue.setLocation(88, 112);
-        targetPiratesLabelValue.setSize(53, 13);
-        targetPiratesLabelValue.setTabIndex(11);
+        targetPiratesLabelValue.setLocation(88, 117);
+        //targetPiratesLabelValue.setSize(53, 13);
         //targetPiratesLabelValue.setText("Abundant");
 
-        targetDistanceLabel.setAutoSize(true);
-        targetDistanceLabel.setFont(FontCollection.bold825);
-        targetDistanceLabel.setLocation(8, 128);
-        targetDistanceLabel.setSize(53, 16);
-        targetDistanceLabel.setTabIndex(8);
+        targetDistanceLabel.setLocation(8, 133);
+        //targetDistanceLabel.setSize(53, 16);
         targetDistanceLabel.setText("Distance:");
 
-        targetDistanceLabelValue.setLocation(88, 128);
-        targetDistanceLabelValue.setSize(66, 13);
-        targetDistanceLabelValue.setTabIndex(12);
+        targetDistanceLabelValue.setLocation(88, 133);
+        //targetDistanceLabelValue.setSize(66, 13);
         ///targetDistanceLabelValue.setText("888 parsecs");
 
-        trackButton.setFlatStyle(FlatStyle.FLAT);
-        trackButton.setLocation(160, 140);
-        trackButton.setSize(44, 22);
-        trackButton.setTabIndex(60);
-        trackButton.setText("Track");
-        trackButton.setVisible(false);
-        trackButton.setClick(new EventHandler<Object, EventArgs>() {
-            @Override
-            public void handle(Object sender, EventArgs e) {
-                trackButtonClick();
-            }
-        });
+        for (Label label : labels) {
+            label.setFont(FontCollection.bold825);
+            label.setAutoSize(true);
+            //labels[i].setTabIndex(i * 2);
+            // Fix different fonts sizes
+            label.setTop(label.getTop() + 1); // TODO
+        }
 
-        targetOutOfRangeLabel.setLocation(8, 144);
-        targetOutOfRangeLabel.setSize(144, 13);
-        targetOutOfRangeLabel.setTabIndex(17);
-        targetOutOfRangeLabel.setText("This system is out of range.");
+        for (Label labelValue : labelValues) {
+            labelValue.setFont(FontCollection.regular825);
+            labelValue.setAutoSize(true);
+            //labelValues[i].setTabIndex(i * 2 + 1);
+        }
 
-        warpButton.setFlatStyle(FlatStyle.FLAT);
-        warpButton.setLocation(160, 98);
+        warpButton.setAutoWidth(true);
+        warpButton.setControlBinding(ControlBinding.RIGHT);
+        warpButton.setLocation(164, 103);
         warpButton.setSize(44, 44);
         warpButton.setTabIndex(59);
         warpButton.setText("Warp");
@@ -206,12 +182,28 @@ class TargetSystemPanel extends Panel {
                 warpButtonClick();
             }
         });
-        
-        getControls().addAll(trackButton, nextSystemButton, prevSystemButton, targetOutOfRangeLabel, warpButton,
-                targetGovernmentLabelValue, targetSizeLabelValue, targetTechLevelLabelValue, targetDistanceLabelValue,
-                targetPiratesLabelValue, targetPoliceLabelValue, targetResourceLabelValue, targetDistanceLabel,
-                targetPiratesLabel, targetPoliceLabel, targetResourceLabel, targetGovernmentLabel, targetTechLevelLabel,
-                targetSizeLabel, targetNameLabelValue, targetNameLabel);
+
+        trackButton.setAutoWidth(true);
+        trackButton.setControlBinding(ControlBinding.RIGHT);
+        trackButton.setLocation(164, 145);
+        trackButton.setSize(44, 22);
+        trackButton.setTabIndex(60);
+        trackButton.setText("Track");
+        trackButton.setClick(new EventHandler<Object, EventArgs>() {
+            @Override
+            public void handle(Object sender, EventArgs e) {
+                trackButtonClick();
+            }
+        });
+
+        targetOutOfRangeLabel.setAutoSize(true);
+        targetOutOfRangeLabel.setLocation(8, 149);
+        //targetOutOfRangeLabel.setSize(144, 13);
+        targetOutOfRangeLabel.setText("This system is out of range.");
+
+        getControls().addAll(prevSystemButton, nextSystemButton, warpButton, targetOutOfRangeLabel, trackButton);
+        getControls().addAll(labels);
+        getControls().addAll(labelValues);
     }
 
     private void updateAll() {
@@ -228,7 +220,7 @@ class TargetSystemPanel extends Panel {
             targetSizeLabelValue.setText("");
             targetTechLevelLabelValue.setText("");
             targetGovernmentLabelValue.setText("");
-            targetResourceLabelValue.setText("");
+            targetResourcesLabelValue.setText("");
             targetPoliceLabelValue.setText("");
             targetPiratesLabelValue.setText("");
             targetDistanceLabelValue.setText("");
@@ -243,7 +235,7 @@ class TargetSystemPanel extends Panel {
             targetSizeLabelValue.setText(Sizes[system.getSize().castToInt()]);
             targetTechLevelLabelValue.setText(TechLevelNames[system.getTechLevel().castToInt()]);
             targetGovernmentLabelValue.setText(system.getPoliticalSystem().getName());
-            targetResourceLabelValue.setText(system.isVisited()
+            targetResourcesLabelValue.setText(system.isVisited()
                     ? SpecialResources[system.getSpecialResource().castToInt()]
                     : Unknown);
             targetPoliceLabelValue.setText(ActivityLevels[system.getPoliticalSystem().getActivityPolice().castToInt()]);
