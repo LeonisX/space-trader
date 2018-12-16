eb7a7d06e15e3f06a3d30d1606c6800ec70acb00
Leonis <tv-games@mail.ru>
2018-12-16 08:25:27 +0200

    Quest name for Lottery quest

--- src/main/java/spacetrader/game/quest/LotteryQuest.java
+++ src/main/java/spacetrader/game/quest/LotteryQuest.java
@@ -4,6 +4,8 @@ import spacetrader.game.Game;
 import spacetrader.game.enums.Difficulty;
 import spacetrader.game.quest.enums.QuestState;
 import spacetrader.game.quest.enums.Repeatable;
+import spacetrader.stub.ArrayList;
+import spacetrader.util.Functions;
 
 import static spacetrader.game.quest.enums.EventName.*;
 import static spacetrader.game.quest.enums.MessageType.ALERT;
@@ -14,6 +16,10 @@ class LotteryQuest extends AbstractQuest {
     private static final Repeatable REPEATABLE = DISPOSABLE;
     private static final int OCCURRENCE = 0;
     private static final int CASH_TO_SPEND = -1000;
+
+    private static final String[] QUESTS = {
+            "Get a prize at the ^1 spaceport."
+    };
 
     private static final QuestDialog[] DIALOGS = new QuestDialog[]{
             new QuestDialog(ALERT, "Lottery Winner", "You are lucky! While docking on the space port, you receive a message that you won 1000 credits in a lottery. The prize had been added to your account.")
@@ -34,12 +40,20 @@ class LotteryQuest extends AbstractQuest {
         getTransitionMap().put(AFTER_GAME_INITIALIZE, this::onAfterGameInitialize);
         getTransitionMap().put(ON_BEFORE_SPECIAL_BUTTON_SHOW, this::onBeforeSpecialButtonShow);
         getTransitionMap().put(ON_SPECIAL_BUTTON_CLICKED, this::onSpecialButtonClicked);
+        getTransitionMap().put(ON_GET_QUESTS_STRINGS, this::onGetQuestsStrings);
     }
 
     @Override
     public void registerListener() {
         getTransitionMap().keySet().forEach(this::registerOperation);
         log.fine("registered");
+    }
+
+    @SuppressWarnings("unchecked")
+    private void onGetQuestsStrings(Object object) {
+        String title = Functions.stringVars(QUESTS[0], Game.getStarSystem(getPhase(0).getStarSystemId()).getName());
+        ((ArrayList<String>) object).add(title);
+        log.fine(title);
     }
 
     private void onAfterGameInitialize(Object object) {
